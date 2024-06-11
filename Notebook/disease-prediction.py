62fcb56c7e3d44c8d40b93dad9b97c638cc370a7
RoomanAbdulLatif <roomanabdullatif@gmail.com>
2024-06-11 20:19:25 +0500

    Converting categorical labels in the 'symptom' column into columns and these columns contain 0 or 1 corresponding to disease by using LabelEncoder and OneHotEncoder technique.

--- Notebook/disease-prediction.py
+++ Notebook/disease-prediction.py
@@ -67,3 +67,45 @@ file_path = 'Notebook\Dataset\cleaned_data.csv' # reading the cleaned dataset
 df = pd.read_csv(file_path, header=None, encoding='latin1')
 df.columns = ['disease', 'symptom', 'occurrence_count']
 print(df.head())
+
+#Replace all float('nan') with np.nan (standard way to represent missing values in pandas)
+df.replace(float('nan'), np.nan, inplace=True)
+#Removing rows having missing value(np.nan)
+df.dropna(inplace=True)
+
+#total number of unique symtoms 
+n_unique = len(df['symptom'].unique())
+print(n_unique)
+#data type of columns
+print(df.dtypes)
+
+
+#convert categorical labels in the 'symptom' column into columns and these columns will contain boolean labels by using LabelEncoder and OneHotEncoder technique. This process is useful when we have categorical data and need to convert it into a numerical format for certain machine learning algorithms
+from sklearn import preprocessing
+#LabelEncoder from sklearn.preprocessing to convert categorical labels in the 'symptom' column of a DataFrame into integer-encoded labels
+from sklearn.preprocessing import LabelEncoder
+#  One-hot Encode the Integer-encoded Labels using OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+# Initialize the LabelEncoder
+label_encoder = LabelEncoder()
+# Fit and transform the 'symptom' column
+integer_encoded = label_encoder.fit_transform(df['symptom'])
+print(integer_encoded)
+#  One-hot Encode the Integer-encoded Labels using OneHotEncoder
+# Initialize the OneHotEncoder with sparse_output set to False
+onehot_encoder = OneHotEncoder(sparse_output=False)
+# Reshape integer_encoded to a 2D array as required by OneHotEncoder
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+print(onehot_encoded)
+print(onehot_encoded[0])
+cols = np.asarray(df['symptom'].unique())
+print(cols)
+
+# Create a new dataframe to save OHE labels
+df_ohe = pd.DataFrame(columns = cols)
+print(df_ohe.head())
+for i in range(len(onehot_encoded)):
+    df_ohe.loc[i] = onehot_encoded[i]
+print(df_ohe.head())
